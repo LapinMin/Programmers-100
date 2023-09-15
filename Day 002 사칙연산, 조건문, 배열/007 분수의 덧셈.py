34e009a4a35a6f64bcc1d892db31bac960f2a81c
LapinMin <lapinuit966@gmail.com>
2023-09-15 14:42:37 +0900

    [0915] Day 002 코드 보완점

--- Day 002 사칙연산, 조건문, 배열/007 분수의 덧셈.py
+++ Day 002 사칙연산, 조건문, 배열/007 분수의 덧셈.py
@@ -8,3 +8,5 @@ def solution(numer1, denom1, numer2, denom2):
     denom = denom1 * denom2
     
     return [numer // gcd(numer, denom), denom // gcd(numer, denom)]
+
+    # // 연산자로 정수 나눗셈 해도 되지만 어차피 두 수의 최대공약수로 나눠 소수점이 발생하지 않으므로 / 연산자를 사용해도 무관
--- Day 002 사칙연산, 조건문, 배열/008 배열 두 배 만들기.py
+++ Day 002 사칙연산, 조건문, 배열/008 배열 두 배 만들기.py
@@ -2,3 +2,5 @@ def solution(numbers):
     for i in range(len(numbers)):
         numbers[i] *= 2
     return numbers
+
+    #return [i * 2 for i in numbers] 로 코드 축약 가능
